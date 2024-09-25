551993f5ea8fb59c4e4cb38235eb0458a5843a79
EVSOLOVIEVA01 <hottabisch1@gmail.com>
2024-09-26 00:50:57 +0300

    Update module_1_3.1.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "HELEN"
 print(name, type(name))
 name = age = 51
 print(name)
-name = new_age = (51-10)
+name = new_age = (age-10)
 print(new_age)
 helen_is_student = True
 print(helen_is_student)
